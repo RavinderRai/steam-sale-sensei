77714ac82acfc70543ad9279593617c00b890db4
Ravi Rai <ravi.b.rai@outlook.com>
2024-06-12 21:35:11 -0300

    Add files via upload

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -193,80 +193,110 @@ def main():
     The xgboost classifier will predict if the game goes on sale within 2 days
     If it doesn't, then we predict how many months until it does, rounding to the nearest int.
     """
-    st.title("Steam Game Search")
+    st.markdown(
+        """
+        <h1 style='text-align: center; color: #4FC3F7;'>Steam Game Sale Prediction</h1>
+        """, 
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        <div style='text-align: center;'>
+            <p>Not sure if you want to buy a new release at full price? No problem!</p>
+            <p>Here you can predict how long you'll have to wait for a game to go on sale.</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    predictions_col, box_art_col = st.columns(2)
 
-    # Input text box for user to enter a game name
-    game_name = st.text_input("Search for a relevant game:")
 
-    if game_name:
-        # Query the games using the input text
-        query_results_dct = query_games(game_name)
+    with predictions_col:
+        # Input text box for user to enter a game name
+        game_name = st.text_input("Search for a relevant game:")
+
+        if game_name:
+            # Query the games using the input text
+            query_results_dct = query_games(game_name)
 
 
-        # Display the list of game names (keys of the output dictionary)
-        if query_results_dct:
-            st.write("We found the following:")
-            selected_game = st.selectbox("Select a game:", list(query_results_dct.keys()))
-            
-            if st.button("Predict how long until sale"):
-                steam_app_id = query_results_dct[selected_game]
+            # Display the list of game names (keys of the output dictionary)
+            if query_results_dct:
+                # empty spacing for aesthetics
+                st.write("")
+                selected_game = st.selectbox("We found the following:", list(query_results_dct.keys()))
+                
+                if st.button("Estimate Sale Date"):
+                    steam_app_id = query_results_dct[selected_game]
 
-                game_id = get_game_id(steam_app_id, api_key)
-                any_deals_game_info = get_all_game_info(game_id)
+                    game_id = get_game_id(steam_app_id, api_key)
+                    any_deals_game_info = get_all_game_info(game_id)
 
-                #boxart = any_deals_game_info['assets']['boxart']
+                    boxart = any_deals_game_info['assets']['boxart']
 
-                steam_game_details = steam_client.apps.get_app_details(
-                    app_id=steam_app_id, 
-                    country='US', 
-                    filters='basic,categories'
-                )
+                    steam_game_details = steam_client.apps.get_app_details(
+                        app_id=steam_app_id, 
+                        country='US', 
+                        filters='basic,categories'
+                    )
 
-                achievements, supported_languages, mac, linux, categories, tags, release_date = get_game_data(steam_app_id, any_deals_game_info, steam_game_details)
-                # use a dict with proper column order as preprocessing steps, and then convert to dataframe.
-                selected_game_dct = {
-                    'Achievements': achievements, 'Supported languages': supported_languages, 
-                    'Mac': mac, 'Linux': linux, 'Categories': categories, 
-                    'Tags': tags, 'ReleaseDate': release_date
-                }
-                selected_game_df = pd.DataFrame([selected_game_dct])
-                selected_game_df = preprocess_game(selected_game_df)
+                    achievements, supported_languages, mac, linux, categories, tags, release_date = get_game_data(steam_app_id, any_deals_game_info, steam_game_details)
+                    # use a dict with proper column order as preprocessing steps, and then convert to dataframe.
+                    selected_game_dct = {
+                        'Achievements': achievements, 'Supported languages': supported_languages, 
+                        'Mac': mac, 'Linux': linux, 'Categories': categories, 
+                        'Tags': tags, 'ReleaseDate': release_date
+                    }
+                    selected_game_df = pd.DataFrame([selected_game_dct])
+                    selected_game_df = preprocess_game(selected_game_df)
 
-                # we need to one hot encode the multilabel columens and remove originals after
-                one_hot_tags = mlb_tag.transform(selected_game_df['Tags'])
-                one_hot_tags_df = pd.DataFrame(one_hot_tags, columns=mlb_tag.classes_)
+                    # we need to one hot encode the multilabel columens and remove originals after
+                    one_hot_tags = mlb_tag.transform(selected_game_df['Tags'])
+                    one_hot_tags_df = pd.DataFrame(one_hot_tags, columns=mlb_tag.classes_)
 
-                one_hot_cats = mlb_cat.transform(selected_game_df['Categories'])
-                one_hot_cats_df = pd.DataFrame(one_hot_cats, columns=mlb_cat.classes_)
+                    one_hot_cats = mlb_cat.transform(selected_game_df['Categories'])
+                    one_hot_cats_df = pd.DataFrame(one_hot_cats, columns=mlb_cat.classes_)
 
-                selected_game_df = selected_game_df.drop(columns=['Categories', 'Tags'])
-                selected_game_df = pd.concat([selected_game_df, one_hot_cats_df, one_hot_tags_df], axis=1)
+                    selected_game_df = selected_game_df.drop(columns=['Categories', 'Tags'])
+                    selected_game_df = pd.concat([selected_game_df, one_hot_cats_df, one_hot_tags_df], axis=1)
 
-                # these were duplicate columns discovered when one-hot-encoding, we removed them for simplicity
-                selected_game_df = selected_game_df.drop(columns=['Co-op', 'PvP'])
+                    # these were duplicate columns discovered when one-hot-encoding, we removed them for simplicity
+                    selected_game_df = selected_game_df.drop(columns=['Co-op', 'PvP'])
 
-                # we need to convert the data to be inputted into xgboost models 
-                input_data = xgb.DMatrix(selected_game_df)
-                discount_on_release = xgboost_clf.predict(input_data)
+                    # we need to convert the data to be inputted into xgboost models 
+                    input_data = xgb.DMatrix(selected_game_df)
+                    discount_on_release = xgboost_clf.predict(input_data)
 
-                # if discount_on_release is less than 0.5 
-                # then the game will be on sale within 2 days
-                # else we calculate how long until it does go on sale (months)
-                if discount_on_release < 0.5:
-                    st.write('Will be on sale within the first 2 days')
-                else:
-                    time_until_discount = xgboost_reg.predict(input_data)
-                    months_until_sale = np.expm1(time_until_discount[0])
-                    months_until_sale = round(months_until_sale)
-
-                    # plural gramer
-                    if months_until_sale == 1:
-                        st.write(f"The game will go in sale in {months_until_sale} month.")
+                    # if discount_on_release is less than 0.5 
+                    # then the game will be on sale within 2 days
+                    # else we calculate how long until it does go on sale (months)
+                    if discount_on_release < 0.5:
+                        sale_prediction = 'less than a week'
                     else:
-                        st.write(f"The game will go in sale in {months_until_sale} months.")
+                        time_until_discount = xgboost_reg.predict(input_data)
+                        months_until_sale = np.expm1(time_until_discount[0])
+                        months_until_sale = round(months_until_sale)
 
-        else:
-            st.write("No results found for the entered game name.")
+                        # plural gramer
+                        if months_until_sale == 1:
+                            sale_prediction = f"{months_until_sale} month"
+                        else:
+                            sale_prediction = f"{months_until_sale} months"
+
+                    st.write("")
+                    st.write("")
+                    st.markdown("<p style='font-size: 24px; text-align: center;'>Will go on sale in:</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p style='font-size: 32px; font-weight: bold; text-align: center;'>{sale_prediction}</p>", unsafe_allow_html=True)
+
+            else:
+                st.write("No results found for the entered game name.")
+
+            with box_art_col:
+                try:
+                    if 'boxart' in locals():
+                        st.image(boxart, caption='Game Box Art')
+                except Exception:
+                    pass
 
 if __name__ == "__main__":
     main()
